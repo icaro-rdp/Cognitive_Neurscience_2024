00e1393328ac6d6592cc4f3d61b62bfa8af08c20
Icaro Re Depaolini <igher97@gmail.com>
2024-04-17 16:02:51 +0200

    Add data analysis module with initial code

---
+++ data_analysis/main.py
@@ -0,0 +1,11 @@
+import pyphysio as ph
+import pyphysio.filters as flt
+import pyphysio.specialized.heart as heart_tools
+from pyphysio.indicators import compute_indicators
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+datadir = 'data_analysis/data/'
+
