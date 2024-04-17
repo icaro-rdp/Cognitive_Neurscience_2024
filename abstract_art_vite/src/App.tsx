ef242b1c7a75775cb979d741091707eb698a1133
Icaro Re Depaolini <igher97@gmail.com>
2024-04-17 11:26:45 +0200

    Add Supabase package and insertData function to App.tsx

--- abstract_art_vite/src/App.tsx
+++ abstract_art_vite/src/App.tsx
@@ -1,7 +1,33 @@
-import "./App.css";
+import { createClient } from "@supabase/supabase-js";
+import { useCallback } from "react";
 
 function App() {
-  return <></>;
+  const supabaseUrl = import.meta.env.VITE_SUPA_URL;
+  const supabaseKey = import.meta.env.VITE_SUPA_KEY;
+
+  const supabase = useCallback(
+    () => createClient(supabaseUrl, supabaseKey),
+    [supabaseUrl, supabaseKey]
+  );
+
+  async function insertData(
+    userID: number,
+    paintingID: number,
+    rating: number
+  ) {
+    console.log(userID, paintingID, rating);
+    console.log(supabase());
+    // const { data, error } = await supabase()
+    //   .from("SAM")
+    //   .insert({ userID: userID, paintingID: paintingID, rating: rating });
+    // console.log(data, error);
+  }
+
+  return (
+    <>
+      <button onClick={() => insertData(1, 1, 5)}>Insert Data</button>
+    </>
+  );
 }
 
 export default App;
