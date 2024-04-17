0f58a4008ff6e0c0a3db2a6ce2ad37f7fcc9f6eb
Icaro Re Depaolini <igher97@gmail.com>
2024-04-17 16:11:22 +0200

    Add data analysis functionality to main.py

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -8,4 +8,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 datadir = 'data_analysis/data/'
+filename = 'bitalino.txt'
+id_signal = 5
+fsamp = 1000
 
+data = pd.read_table(f'{datadir}/{filename}',
+                     skiprows=3,
+                     sep='\t')
+
+data_values = data.values
+
+signal = ph.create_signal(data_values[:,id_signal], sampling_freq=fsamp)
+signal.p.plot()
+plt.xlim(5,20)
+plt.show()
