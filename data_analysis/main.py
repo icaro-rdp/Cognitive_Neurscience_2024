267114c89359d1a89f2d8fa493a087953a21eb38
Icaro Re Depaolini <igher97@gmail.com>
2024-05-04 11:03:12 +0200

    trimmed ecg

--- data_analysis/main.py
+++ data_analysis/main.py
@@ -4,12 +4,15 @@ import pyphysio.specialized.heart as heart_tools
 from pyphysio.indicators import compute_indicators
 import pandas as pd
 import os
-import uuid
 
-def analyze_ecg(filename,folder_path='data_analysis/data/'):
+
+def analyze_ecg(filename,folder_path='data_analysis/segmented/'):
     print(f'Analyzing {filename}')
     title_without_ext = filename.split('.')[0]
-    userID, paintingID, mode = title_without_ext.split('_')
+    userID = title_without_ext.split('_')[0] 
+    paintingID = title_without_ext.split('_')[1] if len(title_without_ext.split('_')) == 3 else '0'
+    mode = title_without_ext.split('_')[2] if len(title_without_ext.split('_')) == 3 else 'B'
+    Id = f'{userID}_{paintingID}_{mode}'
     mode = 'Baseline' if mode == 'B' else 'Stress'
 
     # Read the data from the file
@@ -21,6 +24,7 @@ def analyze_ecg(filename,folder_path='data_analysis/data/'):
     ecg_values = data.values[:,5]
 
     signal = ph.create_signal(ecg_values, sampling_freq=1000)
+    
 
     # Compute HRV indicators
     td_indicators = heart_tools.preset_hrv_td() #time-domain indicators
@@ -33,18 +37,29 @@ def analyze_ecg(filename,folder_path='data_analysis/data/'):
     indicators_td = compute_indicators(td_indicators, ibi)
     indicators_fd = compute_indicators(fd_indicators, ibi.p.resample(4))
 
-    all_indicators = {'Id':uuid.uuid4(), 'userId': userID, 'paintingId': paintingID, 'mode': mode,**indicators_td, **indicators_fd}
+    all_indicators = {'Id': Id, 'userId': userID, 'paintingId': paintingID, 'mode': mode,**indicators_td, **indicators_fd}
     return all_indicators
 
+def segment_txt_file(filename,new_name,t_start,t_end, folder_path='data_analysis/raw_data/',output_folder='data_analysis/segmented', ):
+    sample_rate = 1000
+    # Read the data from the file
+    data = pd.read_table(f'{folder_path}/{filename}',
+                         skiprows=3,
+                         sep='\t')
+    trimmed_data = data.iloc[t_start*sample_rate:t_end*sample_rate]
+    trimmed_data.to_csv(f'{output_folder}/{new_name}.txt', sep='\t', index=False)
+   
 def main():
     # Get all the files in the data folder
-    files = os.listdir('data_analysis/data')
+    files = os.listdir('data_analysis/segmented')
+    
     all_indicators = []
     for file in files:
         if file.endswith('.txt'):
             all_indicators.append(analyze_ecg(file))
     df = pd.DataFrame(all_indicators)
-    df.to_csv('data_analysis/hrv.csv', index=False)
+    sorted_df = df.sort_values(by=['userId', 'paintingId', 'mode'])
+    sorted_df.to_csv('data_analysis/hrv.csv', index=False)
 
 if __name__ == '__main__':
     main()
