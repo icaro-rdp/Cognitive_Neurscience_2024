bd81f64cfef4523486567555e8f80b450e6c8cf4
Icaro Re Depaolini <igher97@gmail.com>
2024-05-05 12:06:42 +0200

    Initial test with SVM & RandomForest

--- data_analysis/hrv_classifier.py
+++ data_analysis/hrv_classifier.py
@@ -1,63 +1,80 @@
 import pandas as pd
 from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import LeaveOneGroupOut
-from sklearn.model_selection import cross_val_score
-import numpy as np
+from sklearn.model_selection import LeaveOneOut
+from sklearn.metrics import accuracy_score
 import os
 import sys
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 # Set the working directory
 os.chdir('data_analysis')
 
-# Load HRV data (replace 'hrv.csv' with your actual file path)
+# Load HRV data 
 hrv_data = pd.read_csv('hrv_baseline_balanced.csv')
-
-# Load painting ratings data (replace 'ratings.csv' with your actual file path)
 ratings_data = pd.read_csv('ratings.csv')
-
 merged_data = hrv_data.merge(ratings_data, on=['Id', 'userId', 'paintingId'], how='inner')
 
-
-# Or Numpy arrays?
+# Drop unnecessary columns
 X = merged_data.drop(['Id','userId', 'paintingId', 'mode', 'rating'], axis=1)
-y = merged_data['rating']
+y = merged_data['rating'].apply(lambda x: 1 if x > 5 else 0)
+
+# Check corr matrix
+plt.figure(figsize=(12, 10))
+cor = X.corr()
+sns.heatmap(cor, annot=True, cmap=plt.cm.CMRmap_r)
+
+
+def correlated_features(dataset, threshold):
+    col_corr = set()  # Set of all the names of correlated columns
+    corr_matrix = dataset.corr()
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i):
+            if abs(corr_matrix.iloc[i, j]) > threshold: # we are interested in absolute coeff value
+                colname = corr_matrix.columns[i]  # getting the name of column
+                col_corr.add(colname)
+    return col_corr
+
+corr_features = correlated_features(X, 0.8)
+# Drop correlated features
+X = X.drop(corr_features, axis=1)
 
 # Standardize features
 scaler = StandardScaler()
+X = scaler.fit_transform(X)
+y = y.values
+
+# Leave-one-out cross-validation
+loo = LeaveOneOut()
+
+# Initialize models
+svm_model = SVC(kernel='poly', C=1.0)  
+
+svm_scores = []
+for i, (train_index, test_index) in enumerate(loo.split(X)):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    # Train models
+    svm_model.fit(X_train, y_train)
+    svm_pred = svm_model.predict(X_test)
+    svm_scores.append(accuracy_score(y_test, svm_pred))
 
 
-# Get subject IDs as groups for LOSO
-groups = merged_data['Id'] 
+print(f'SVM Average accuracy: {np.mean(svm_scores)}')
 
-# Leave-One-Subject-Out cross-validation
-logo = LeaveOneGroupOut()
+# Random Forest
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_scores = []
 
-# Initialize SVM classifier
-svm_model = SVC(kernel='linear', C=1.0)  
-
-# Perform Leave-One-Subject-Out cross-validation
-for train_index, test_index in logo.split(X, y, groups):
-    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-    
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-        
-    svm_model.fit(X_train_scaled, y_train)
-    accuracy = svm_model.score(X_test_scaled, y_test)
-    print(f'Accuracy for subject {groups[test_index].values[0]}: {accuracy}')
-
-# Evaluate model performance using LOSO and regularization
-
-scores = cross_val_score(svm_model, X, y, groups=groups, cv=logo)
-print(f'Mean Accuracy: {scores.mean()}')
-
-# Train final model on full dataset
-svm_model.fit(X, y)
+for i, (train_index, test_index) in enumerate(loo.split(X)):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    # Train models
+    rf_model.fit(X_train, y_train)
+    rf_pred = rf_model.predict(X_test)
+    rf_scores.append(accuracy_score(y_test, rf_pred))
 
 
-
-
-
-
+print(f'Random forest Average accuracy: {np.mean(rf_scores)}')
